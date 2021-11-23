c7941865480f10764f3e033433dde6415bea1cf6
mario pereira <marioscpereira@gmail.com>
2021-11-23 19:13:20 +0000

    changed seeds, added user_id for each plant

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,9 +19,9 @@ user3 = User.new(email: "Kristina@qualqer.com", password: "123456")
 user3.save
 user4 = User.new(email: "Alexandro@qualqer.com", password: "123456")
 user4.save
-plant1 = Plant.new(species: "cactus", name: "Bob", description: "nice plant")
+plant1 = Plant.new(species: "cactus", name: "Bob", description: "nice plant", user_id: user1.id)
 plant1.save
-plant2 = Plant.new(species: "cactus", name: "Rita", description: "nice Rita")
+plant2 = Plant.new(species: "cactus", name: "Rita", description: "nice Rita", user_id: user1.id)
 plant2.save
-plant3 = Plant.new(species: "fig tree", name: "tusk", description: "nice tusk")
+plant3 = Plant.new(species: "fig tree", name: "tusk", description: "nice tusk", user_id: user1.id)
 plant3.save
