3c9ecfef5a6020cc1616e421c834fa85cf489915
paulo araujo <paulo.pp.araujo@gmail.com>
2021-11-23 12:36:34 +0000

    routes, plants controller, begin pundit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "DESTROY DATA"
+Plant.delete_all
+User.delete_all
+puts "CREATING DATA"
+
+
+user1 = User.new(email: "Paulo@qualqer.com", password: "123456")
+user1.save
+
+user2 = User.new(email: "Mario@qualqer.com", password: "123456")
+user2.save
+user3 = User.new(email: "Kristina@qualqer.com", password: "123456")
+user3.save
+user4 = User.new(email: "Alexandro@qualqer.com", password: "123456")
+user4.save
+plant1 = Plant.new(species: "cactus", name: "Bob", description: "nice plant", user_id: 1)
+plant1.save
+plant2 = Plant.new(species: "cactus", name: "Rita", description: "nice Rita", user_id: 1)
+plant2.save
+plant3 = Plant.new(species: "fig tree", name: "tusk", description: "nice tusk", user_id: 1)
+plant3.save
