41c5c8b30fb0191d128df0f0595ef30c931407b7
mario pereira <marioscpereira@gmail.com>
2021-11-23 18:10:17 +0000

    Created bookings model/controller/views. Changed navbar links do myPlants and myBookings while loged in and created links to dashboard (bookings list) and dashboard_myplants (plants list)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :owned_plants, class_name: "Plant", foreign_key: "user_id"
   has_many :bookings
   has_many :plants, through: :bookings
 end
