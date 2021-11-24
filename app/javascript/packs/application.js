408373926093aa6fca583b15e1d7bfc1ad23b309
paulo araujo <paulo.pp.araujo@gmail.com>
2021-11-24 12:33:44 +0000

    Map container with Javascript initiated

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,14 +21,16 @@ require("channels")
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-
+import 'mapbox-gl/dist/mapbox-gl.css';
 // External imports
 import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,3 +1,5 @@
 class Plant < ApplicationRecord
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
   belongs_to :user
 end
