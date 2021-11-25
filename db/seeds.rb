7267ff9e751f3b7f26bccbada5fd0b85c50eea7f
Alexandro Nussbaumer <90007596+alexandroNussbaumer@users.noreply.github.com>
2021-11-25 11:57:41 +0000

    Added Address and Description to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,28 +6,39 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "DESTROY DATA"
+Booking.delete_all
 Plant.delete_all
 User.delete_all
 puts "CREATING DATA"
 
-user1 = User.new(email: "Paulo@qualqer.com", password: "123456")
+user1 = User.new(email: "paulo@qualqer.com", password: "123456")
 user1.save
 
-user2 = User.new(email: "Mario@qualqer.com", password: "123456")
+user2 = User.new(email: "mario@qualqer.com", password: "123456")
 user2.save
-user3 = User.new(email: "Kristina@qualqer.com", password: "123456")
+user3 = User.new(email: "kristina@qualqer.com", password: "123456")
 user3.save
-user4 = User.new(email: "Alexandro@qualqer.com", password: "123456")
+user4 = User.new(email: "alexandro@qualqer.com", password: "123456")
 user4.save
-plant1 = Plant.new(species: "cactus", name: "Bob", description: "nice plant", user_id: user1.id)
-file = URI.open('https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.sensorytrust.org.uk%2Fuploads%2Fimages%2Fgarden-flowers-and-seating.jpg&f=1&nofb=1')
+plant1 = Plant.new(species: "Aloe Vera", name: "Cindy", address: "1600 Pennsylvania Avenue NW Washington, D.C. 20500 U.S.", description: "From curing heartburn to sunburn, you'll want to have cindy around. She's tought on the outside, soft on the inside and just makes a trustworthy companion to weather life's most unexpected outcomes", user_id: user1.id)
+file = URI.open('https://www.kendallsflorist.co.uk/wp-content/uploads/2016/01/Aloe-Vera-Plant-1.jpg')
 plant1.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 plant1.save
-plant2 = Plant.new(species: "cactus", name: "Rita", description: "nice Rita", user_id: user1.id)
-file = URI.open('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Flifezshining.com%2Fwp-content%2Fuploads%2F2020%2F12%2Fimage-9.jpg&f=1&nofb=1')
+plant2 = Plant.new(species: "Monstera", name: "Rita", address: "55 Rue du Faubourg Saint-Honoré, 75008 Paris, France", description: "Rita is about bringing the rainforest to you, not you to the rainforest. She's about abundance, joy and splendor. She's truly a breath of fresh air.", user_id: user1.id)
+file = URI.open('https://urbanjungle.pt/wp-content/uploads/2020/02/Monstera-deliciosa-XL.jpg')
 plant2.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 plant2.save
-plant3 = Plant.new(species: "fig tree", name: "tusk", description: "nice tusk", user_id: user1.id)
-file = URI.open('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0260%2F3037%2F4957%2Fproducts%2Fsignature-white-pots-with-plants_eae028fd-0436-4e54-8243-ad4f5e4e2627_1200x1200.jpg%3Fv%3D1573582351&f=1&nofb=1')
+plant3 = Plant.new(species: "Dracaena", name: "Tusk", address: "Bundesplatz 3, 3003 Bern, Switzerland", description: "He might come from the most neutral country in the world but rest asured, Tusk has character and will play a vital role in the outcome of your day.", user_id: user1.id)
+file = URI.open('https://cdn.thecolvinco.com/photos_cache_thumb/xl-drake/xl-drake-6980797f-7b94-4b35-9820-3adebb3f046e.jpg')
 plant3.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 plant3.save
+plant4 = Plant.new(species: "Yucca", name: "Mona", address: "1 Sheikh Mohammed bin Rashid Blvd - Downtown Dubai - Dubai - United Arab Emirates", description: "She comes from the Desert. She's tought as nails. She was also seen on The Good, the Bad and the Ugly", user_id: user1.id)
+file = URI.open('https://tpn-1.s3.eu-west-2.amazonaws.com/media/217/217-Page38-anca-gabriela-zosin-f3e9p4-lqRw-unsplash.jpeg')
+plant4.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+plant4.save
+plant5 = Plant.new(species: "Ficus", name: "Bruce", address: "4 Jingshan Front St, Dongcheng, Beijing Shi, China, 100886", description: "Don't let his looks decieve you. Bruce is a ninja. Adapts to any corner, lounge, bathroom and bedroom. His moto: Be water my friend", user_id: user1.id)
+file = URI.open('https://bloomscape.com/wp-content/uploads/2020/05/bloomscape_ficus-altissima-std_charcoal.jpg')
+plant5.photos.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+plant5.save
+
+puts "#{Plant.count} plants created"
